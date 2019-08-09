d085dec562ce930e62ed61dd2dc2edbe9ab2c704
tarrinros <tarindis@gmail.com>
2019-08-09 21:21:32 +0300

    Add Portfolio and PortfolioItem costructors

--- app/javascript/components/PortfolioItem.js
+++ app/javascript/components/PortfolioItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class Portfolio extends Component {
+class PortfolioItem extends Component {
   constructor(props) {
     super(props)
   }
@@ -12,4 +12,4 @@ class Portfolio extends Component {
   }
 }
 
-export default Portfolio
+export default PortfolioItem
