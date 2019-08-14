3604ce56f0e3286a342e53ef0b7a2a51f40b9099
tarrinros <tarindis@gmail.com>
2019-08-14 15:47:45 +0300

    Change post url for axios in Portfolio Container

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -23,7 +23,7 @@ class PortfolioContainer extends Component {
   }
 
   handleChange(e) {
-    axios.post('http://localhost:3000/search', {
+    axios.post('https://cryptocur-portfolio.herokuapp.com/search', {
       search: e.target.value
     })
       .then((data) => {
@@ -52,7 +52,7 @@ class PortfolioContainer extends Component {
     let currency = this.state.active_currency
     let amount = this.state.amount
 
-    axios.post('http://localhost:3000/calculate', {
+    axios.post('https://cryptocur-portfolio.herokuapp.com/calculate', {
       id: currency.id,
       amount: amount
     })
